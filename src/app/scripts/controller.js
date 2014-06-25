03c4456dd3b86a9bbc265238bdbe0cdc62fcb371
Leland Miller <lelandmiller@gmail.com>
2014-06-25 16:53:26 -0700

    Added save, switched to bower, switched ace editor and all js to angular controller.

--- src/app/scripts/controller.js
+++ src/app/scripts/controller.js
@@ -1,14 +1,35 @@
-var myWiki = angular.module('myWiki', []);
 var core = require('./scripts/core.js');
-files = core.getFileTree();
+var marked = require('marked');
+var fs = require('fs');
+
+var myWiki = angular.module('myWiki', ['ui.ace']);
+
+var files = core.getFileTree();
+
 console.log(files);
 
-function mainController($scope, $http) {
-    $scope.test = "hello";
+function mainController($scope, $http, $sce) {
     $scope.items = files;
+    $scope.currentFilename = "";
+
+    $scope.content = "# Welcome";
+    $scope.preview = "";
+
     $scope.loadFile = function (filename) {
+        $scope.currentFilename = filename;
+        $scope.content = fs.readFileSync(filename, {
+            encoding: 'utf8'
+        });
         console.log(filename);
     };
+
+    $scope.aceChanged = function (e) {
+        console.log($scope.content);
+        $scope.preview = $sce.trustAsHtml(marked($scope.content));
+    };
+
+    $scope.saveFile = function () {
+        fs.writeFileSync($scope.currentFilename, $scope.content);
+        console.log('save clicked');
+    };
 }
-
-
