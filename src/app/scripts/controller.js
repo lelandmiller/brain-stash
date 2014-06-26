4fdadc7c9664218dc027a52b8fb646795b8e455b
Leland Miller <lelandmiller@gmail.com>
2014-06-26 15:23:29 -0700

    Changed theme and style of controller initialization.

--- src/app/scripts/controller.js
+++ src/app/scripts/controller.js
@@ -1,38 +1,39 @@
 var marked = require('marked');
 var fs = require('fs');
 
-var myWiki = angular.module('myWiki', ['ui.ace']);
+var myWikiApp = angular.module('myWiki', ['ui.ace']);
 
 var files = myWikiCore.getFileTree();
 
 console.log(files);
 
-function mainController($scope, $http, $sce) {
-    $scope.items = files;
-    $scope.currentFilename = "";
+myWikiApp.controller('mainController', ['$scope', '$http', '$sce',
+    function($scope, $http, $sce) {
+        $scope.items = files;
+        $scope.currentFilename = "";
+        $scope.content = "";
+        $scope.preview = "";
 
-    $scope.content = "# Welcome";
-    $scope.preview = "";
+        $scope.loadFile = function(entryObject) {
+            var filepath = path.join(entryObject.path, entryObject.title + '.md'),
+                content = "";
+            $scope.currentFilename = filepath;
+            if (fs.existsSync(filepath)) {
+                content = fs.readFileSync(filepath, {
+                    encoding: 'utf8'
+                });
+            }
+            $scope.content = content;
+        };
 
-    $scope.loadFile = function (entryObject) {
-        var filepath = path.join(entryObject.path, entryObject.title + '.md'),
-            content = "";
-        $scope.currentFilename = filepath;
-        if (fs.existsSync(filepath)) {
-            content = fs.readFileSync(filepath, {
-                encoding: 'utf8'
-            });
-        }
-        $scope.content = content;
-    };
+        $scope.aceChanged = function(e) {
+            console.log($scope.content);
+            $scope.preview = $sce.trustAsHtml(marked($scope.content));
+        };
 
-    $scope.aceChanged = function (e) {
-        console.log($scope.content);
-        $scope.preview = $sce.trustAsHtml(marked($scope.content));
-    };
-
-    $scope.saveFile = function () {
-        fs.writeFileSync($scope.currentFilename, $scope.content);
-        console.log('save clicked');
-    };
-}
+        $scope.saveFile = function() {
+            fs.writeFileSync($scope.currentFilename, $scope.content);
+            console.log('save clicked');
+        };
+    }
+]);
